7c52bd2ed5e3f6e5ddb0e017ec911abc7db0d160
Ayman <yaoiotakusama@gmail.com>
2021-10-28 20:52:40 +0530

    Added light and dark theme toggle

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,28 @@
-import preferenceIcon from "../assets/icon-moon.svg"
+import preferDark from "../assets/icon-moon.svg";
+import preferLight from "../assets/icon-sun.svg";
 
 const Header = () => {
+    const setPreference = (e) => {
+        const theme = document.documentElement.getAttribute("data-theme");
+        const img = document.querySelector(".preferenceIcon");
+        const themeText = document.querySelector(".colorPreference");
+        if(theme === "dark") {
+            document.documentElement.setAttribute('data-theme', 'light');
+            img.src = preferDark;
+            themeText.innerText = "Dark";
+        }else {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            img.src = preferLight;
+            themeText.innerText = "Light";
+        }
+    }
+
     return (
         <header>
             <div className="title">devfinder</div>
             <div className="preferenceWrapper">
                 <div className="colorPreference">Light</div>
-                <img className="preferenceIcon" src={preferenceIcon} alt="colorSchemePreferenceIcon"/>
+                <img onClick={setPreference} className="preferenceIcon" src={preferLight} alt="colorSchemePreferenceIcon"/>
             </div>
         </header>
     );
